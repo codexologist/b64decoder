72e72bfe123bd8093c019c786a52a97788604a96
John Elliot Baker <johnelliotbaker@gmail.com>
2019-05-31 11:11:18 -0700

    Init

---
+++ auto_b64_decoder.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name Snahp Automatic Base64 Decoder
+// @namespace Violentmonkey Scripts
+// @match *://forum.snahp.it/*
+// @match *://*/*
+// @version 0.0.0
+// ==/UserScript==
+//
+// @require https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @updateURL https://raw.githubusercontent.com/codexologist/b64decoder/master/auto_b64_decoder.js
+// @downloadURL https://raw.githubusercontent.com/codexologist/b64decoder/master/auto_b64_decoder.js
+
+var SB64D = {};
+
+
+SB64D.re_megaurl = /[^a-z0-9\+\=\:\/\n\.\#\!\-\_\t]/i;
+
+SB64D.validateUrl = function(word)
+{
+    return SB64D.re_megaurl.exec(word)===null;
+}
+
+SB64D.decode64 = function(word)
+{
+    try
+    { 
+        var decoded = atob(word);
+        console.log(word);
+        console.log(decoded);
+        if (!SB64D.validateUrl(decoded)) return false;
+        console.log(1);
+        var recoded = btoa(decoded);
+        if (recoded == word) return decoded;
+        return false;
+    }
+    catch (err) { return false; }
+}
+
+SB64D.decode = function(word)
+{
+    var decoded = SB64D.decode64(word);
+    if (decoded)
+    {
+        return SB64D.decode(decoded);
+    }
+    return word;
+}
+
+$content = $(".content");
+$content.each((index)=>{
+    post = $content[index];
+    for (const word of post.innerText.split(/\s|\n|:/))
+    {
+        if (word.length < 16) { continue; }
+        var decoded = SB64D.decode(word);
+        post.innerHTML = post.innerHTML.replace(word, decoded);
+    }
+})
+
